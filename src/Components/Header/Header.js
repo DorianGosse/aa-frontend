eac36d9e38120816f17a6512449aa5e476eef7af
Dorian Gosse <Dorian.Richard@hotmail.com>
2022-07-06 17:51:08 -0500

    impl header / city selections

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,9 +2,31 @@ import React from 'react'
 import './Header.css'
 
 class Header extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      locations: props.locations,
+      selectedCity: props.locations[0]
+    }
+  }
   render() {
+
+  const handleCityOnClick = location => {
+    this.props.handleCityToggle(location)
+    this.setState({ selectedCity: location })
+  }
+
     return (
-      <></>
+      <div className='weather-header'>
+        {this.state.locations.map(location => (
+          <div
+            className={this.state.selectedCity === location ? 'selected-city' : 'city-toggle'}
+            key={`${location.name}-${location.lat}-${location.lng}`}
+            onClick={() => handleCityOnClick(location)}>
+            {location.name}
+          </div>
+        ))}
+      </div>
     )
   }
 }
--- src/Components/WeatherDisplay/WeatherDisplay.js
+++ src/Components/WeatherDisplay/WeatherDisplay.js
@@ -3,11 +3,28 @@ import './WeatherDisplay.css'
 
 import Header from '../Header'
 
+const cities = [
+  { name: 'Winnipeg', lat: 1234, lng: 1234 },
+  { name: `Halifax`, lat: 3333, lng: 3342 },
+  { name: 'Greenwood', lat: 3342, lng: 435345 }
+]
+
 class WeatherDisplay extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      selectedCity: cities[0]
+    }
+  }
+
   render() {
+    const handleCityToggle = citySelected => {
+      this.setState({ selectedCity: citySelected })
+    }
+
     return (
       <div className='weather-display'>
-        <Header />
+        <Header locations={cities} handleCityToggle={handleCityToggle}/>
       </div>
     )
   }
