750e9555873ca1f87dad368858a793d4d663bfe5
Dorian Gosse <Dorian.Richard@hotmail.com>
2022-07-06 15:11:33 -0500

    Clean up unused react-react-app and impl component structure

---
+++ src/Components/Header/header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './header.css'
+
+class header extends React.Component {
+  render() {
+    return (
+      <></>
+    )
+  }
+}
+
+export default header
---
+++ src/Components/WeatherDisplay/weatherDisplay.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './weatherDisplay.css'
+
+class weatherDisplay extends React.Component {
+  render() {
+    return (
+      <></>
+    )
+  }
+}
+
+export default weatherDisplay
