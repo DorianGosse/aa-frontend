053463543b415b44e57bdc36d303e1130ebe6275
Dorian Gosse <Dorian.Richard@hotmail.com>
2022-07-07 17:41:00 -0500

    impl fetch forcast data and forcast section / fix units

--- src/Components/WeatherDisplay/WeatherDisplay.js
+++ src/Components/WeatherDisplay/WeatherDisplay.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import './WeatherDisplay.css'
 
-import { getWeatherData } from '../../Utils/fetchWeatherData'
+import { getWeatherData, get4DayForcast } from '../../Utils/fetchWeatherData'
+import { getDayOfWeek } from '../../Utils/dateCalculations'
 
 import Header from '../Header'
 
@@ -29,7 +30,7 @@ class WeatherDisplay extends React.Component {
 
   render() {
 
-    const currentDeg = this.state.currentWeather.wind
+    const currentDeg = this.state.currentWeather.main
     const currentConditions = this.state.currentWeather.weather
     const handleCityToggle = citySelected => {
       this.setState({ selectedCity: citySelected })
@@ -43,21 +44,33 @@ class WeatherDisplay extends React.Component {
             <img src={currentConditions && `http://openweathermap.org/img/w/${currentConditions[0].icon}.png`} />
           </div>
           <div className='weather-temp-container'>
-            <div> {currentDeg && currentDeg.deg} <span className='degree'> &deg; </span> </div>
+            <div> {currentDeg && currentDeg.temp.toFixed()} <span className='degree'> &deg; </span> </div>
             <div> {currentConditions && currentConditions[0].main} </div>
           </div>
         </div>
       </div>
     )
 
-    const createWeeklyForecast = () => (
-      <></>
+    const getForecastData = daysInFuture => (
+      <div className='flex-container-column'>
+        <div> {getDayOfWeek(daysInFuture)} </div>
+      </div>
+    )
+
+    const create4DayForecast = () => (
+      <div className='forecast-container'>
+        {getForecastData(1)}
+        {getForecastData(2)}
+        {getForecastData(3)}
+        {getForecastData(4)}
+      </div>
     )
 
     return (
       <div className='weather-display'>
         <Header locations={cities} handleCityToggle={handleCityToggle}/>
         {createTodaysWeather()}
+        {create4DayForecast()}
       </div>
     )
   }
--- src/Utils/fetchWeatherData.js
+++ src/Utils/fetchWeatherData.js
@@ -1,7 +1,7 @@
+  const key = '198d2fc27d0b5ee3b03469c6fa4b7fbc'
 
 export const getWeatherData = async (queryData, setCurrentWeather) => {
-  const key = '198d2fc27d0b5ee3b03469c6fa4b7fbc'
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${queryData.lat}&lon=${queryData.lng}&appid=${key}`
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${queryData.lat}&lon=${queryData.lng}&appid=${key}&units=metric`
   try {
     const response = await fetch(url)
     const data = await response.json()
@@ -10,4 +10,18 @@ export const getWeatherData = async (queryData, setCurrentWeather) => {
   } catch (e) {
     console.log('error fetching weather data', e.message)
   }
+}
+
+export const get4DayForcast = async (queryData, setForcastData) => {
+  const url = `api.openweathermap.org/data/2.5/forecast?lat=${queryData.lat}&lon=${queryData.lng}&appid=${key}&units=metric`
+  try {
+    const response = await fetch(url)
+    const data = await response.json()
+    console.log('forecast data', data)
+    setForcastData(data)
+  }
+  catch (e) {
+    console.log('error fetching forecast data:', e.message)
+  }
+
 }
